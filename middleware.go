86169fa80f89aa0e6f9c19f5819df39808c6cf67
Jannes <jannes.hachmer@gmail.com>
2024-11-25 15:00:44 +0100

    Added middleware.go

---
+++ middleware.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type Middleware func(handlerFunc http.HandlerFunc) http.HandlerFunc
+
+func Chain(f http.HandlerFunc, middlewares ...Middleware) http.HandlerFunc {
+	for _, m := range middlewares {
+		f = m(f)
+	}
+	return f
+}
+
+type LogMessage struct {
+	Path     string
+	Method   string
+	Duration time.Duration
+	Time     time.Time
+}
+
+func NewLogMessage(r *http.Request, startTime time.Time) *LogMessage {
+	return &LogMessage{
+		Path:     r.URL.EscapedPath(),
+		Method:   r.Method,
+		Duration: time.Since(startTime),
+		Time:     time.Now(),
+	}
+}
+
+func Logging(logger *log.Logger) Middleware {
+	return func(handlerFunc http.HandlerFunc) http.HandlerFunc {
+		return func(w http.ResponseWriter, r *http.Request) {
+			defer func() {
+				message := NewLogMessage(r, time.Now())
+				logger.Println(message.Method, message.Path, message.Duration.String())
+			}()
+			handlerFunc(w, r)
+		}
+	}
+}
