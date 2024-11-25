d2bf66564cd5855a55c9af89fe95556368b22f31
Jannes <jannes.hachmer@gmail.com>
2024-11-25 15:03:04 +0100

    added logging middleware to handlers

--- main.go
+++ main.go
@@ -3,16 +3,14 @@ package main
 import (
 	"log"
 	"net/http"
-	"strings"
+	"os"
 )
 
 func main() {
+	logger := log.New(os.Stdout, "goto:", log.LstdFlags)
 	mux := http.NewServeMux()
-	mux.HandleFunc("GET /films/{name}", FileStreamer)
-	mux.HandleFunc("GET /films", ListMovies)
+	mux.HandleFunc("GET /health", Chain(HealthHandler, Logging(logger)))
+	mux.HandleFunc("GET /films/{name}", Chain(FileStreamer, Logging(logger)))
+	mux.HandleFunc("GET /films", Chain(ListMovies, Logging(logger)))
 	log.Fatal(http.ListenAndServe(":8080", mux))
-}
-
-func ValidateMovieFile(filename string) bool {
-	return strings.HasSuffix(filename, ".mp4")
 }
